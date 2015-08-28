1a021f4c83312bb4d2a78b126b0ce8fbbf540947
Heiner Tholen <heiner.tholen@cern.ch>
2015-08-28 13:37:56 +0200

    removing some isomu triggers + histo binning

--- include/VLQSLPS_selectionItems.h
+++ include/VLQSLPS_selectionItems.h
@@ -18,10 +18,10 @@ static const float DPT_2D_CUT_PRESEL = 40.0;
 
 static const vector<shared_ptr<SelectionItem>> SEL_ITEMS_PRESEL {
     shared_ptr<SelectionItem>(new SelDatI("trigger_accept",    "trigger accept",                    2, -.5, 1.5       ,1      )),
-    shared_ptr<SelectionItem>(new SelDatF("leading_jet_pt",    "leading jet p_{T} / 20 GeV",        75, 0, 1500       ,100    )),
-    shared_ptr<SelectionItem>(new SelDatF("primary_lepton_pt", "primary lepton p_{T} / 20 GeV",     75, 0, 1500       ,25     )),
-    shared_ptr<SelectionItem>(new SelDatD("ST",                "ST / 50 GeV",                       100, 0, 5000      ,400    )),
-    shared_ptr<SelectionItem>(new SelDatI("n_btags",           "number of loose btags",             11, -.5, 10.5     ,1      )),
+    shared_ptr<SelectionItem>(new SelDatF("leading_jet_pt",    "leading jet p_{T} / 10 GeV",        75, 0, 750       ,100    )),
+    shared_ptr<SelectionItem>(new SelDatF("primary_lepton_pt", "primary lepton p_{T} / 10 GeV",     75, 0, 750       ,25     )),
+    shared_ptr<SelectionItem>(new SelDatD("ST",                "ST / 25 GeV",                       100, 0, 2500      ,400    )),
+    // shared_ptr<SelectionItem>(new SelDatI("n_btags",           "number of loose btags",             11, -.5, 10.5     ,1      )),
 };
 
 
@@ -32,9 +32,9 @@ static const vector<std::string> PRESEL_TRIGGER_PATHS {
 
     // "HLT_Mu40_eta2p1_PFJet200_PFJet50_v*",
     "HLT_Mu45_eta2p1_v*",
-    "HLT_Mu50_v*",
+    // "HLT_Mu50_v*",
     "HLT_IsoMu24_eta2p1_v*",
-    "HLT_IsoMu27_v*",
+    // "HLT_IsoMu27_v*",
 
     "HLT_PFHT800Emu_v*",
 };
@@ -46,9 +46,9 @@ static const vector<std::string> PRESEL_TRIGGER_PATHS_DATA {
 
     // "HLT_Mu40_eta2p1_PFJet200_PFJet50_v*",
     "HLT_Mu45_eta2p1_v*",
-    "HLT_Mu50_v*",
+    // "HLT_Mu50_v*",
     "HLT_IsoMu24_eta2p1_v*",
-    "HLT_IsoMu27_v*",
+    // "HLT_IsoMu27_v*",
 
     "HLT_PFHT800_v*",
 };
@@ -56,9 +56,9 @@ static const vector<std::string> PRESEL_TRIGGER_PATHS_DATA {
 static const vector<std::string> PRESEL_TRIGGER_PATHS_DATA_ELE_VETO {
     // "HLT_Mu40_eta2p1_PFJet200_PFJet50_v*",
     "HLT_Mu45_eta2p1_v*",
-    "HLT_Mu50_v*",
+    // "HLT_Mu50_v*",
     "HLT_IsoMu24_eta2p1_v*",
-    "HLT_IsoMu27_v*",
+    // "HLT_IsoMu27_v*",
 };
 
 static const vector<std::string> PRESEL_TRIGGER_PATHS_DATA_HAD_VETO {
@@ -68,7 +68,7 @@ static const vector<std::string> PRESEL_TRIGGER_PATHS_DATA_HAD_VETO {
 
     // "HLT_Mu40_eta2p1_PFJet200_PFJet50_v*",
     "HLT_Mu45_eta2p1_v*",
-    "HLT_Mu50_v*",
+    // "HLT_Mu50_v*",
     "HLT_IsoMu24_eta2p1_v*",
-    "HLT_IsoMu27_v*",
+    // "HLT_IsoMu27_v*",
 };
