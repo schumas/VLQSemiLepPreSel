5aaf2bcfaffc1c7e82bcb44ba6f173f2b92c4d91
Heiner Tholen <heiner.tholen@cern.ch>
2015-11-30 09:36:15 +0100

    fixing prefixes

--- python/hadd_presel.py
+++ python/hadd_presel.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from varial.extensions.hadd import Hadd
 import varial.tools
 import sys
@@ -12,15 +14,13 @@ if __name__ == '__main__':
         input_pat, 
         [
             'uhh2.AnalysisModuleRunner.DATA.Run2015D', 
-            'uhh2.AnalysisModuleRunner.MC.MC_TTbar',
-            'uhh2.AnalysisModuleRunner.MC.MC_WJets',
+            'uhh2.AnalysisModuleRunner.MC.TTbar',
+            'uhh2.AnalysisModuleRunner.MC.WJets',
             'uhh2.AnalysisModuleRunner.MC.DYJets',
             'uhh2.AnalysisModuleRunner.MC.QCD',
-            'uhh2.AnalysisModuleRunner.MC.MC_QCD',
             'uhh2.AnalysisModuleRunner.MC.SingleT',
             'uhh2.AnalysisModuleRunner.MC.TpB_TH_LH_M0700_lepDecay',
-            'uhh2.AnalysisModuleRunner.MC.TpTp_M-700_lepDecay',
-            'uhh2.AnalysisModuleRunner.MC.TpTp_M-1700_lepDecay',
+            'uhh2.AnalysisModuleRunner.MC.TpB_TH_LH_M1700_lepDecay',
         ], 
         add_aliases_to_analysis=False,
     )
